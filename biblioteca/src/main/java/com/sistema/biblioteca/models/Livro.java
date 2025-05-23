7021d5686acdbb5b965fe124006f2183fcce9d08
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-05-23 14:34:38 -0300

    Criação do pacote models e da classe Livro

---
+++ biblioteca/src/main/java/com/sistema/biblioteca/models/Livro.java
@@ -0,0 +1,11 @@
+package com.sistema.biblioteca.models;
+
+import com.sistema.biblioteca.enums.Tamanho;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro {
+    
+}
