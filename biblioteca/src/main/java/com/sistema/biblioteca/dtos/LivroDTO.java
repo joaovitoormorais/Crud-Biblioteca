997c84929d988a7889208ab5a1e9bf650aa09f8d
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-05-25 15:11:18 -0300

    criação do pacote dtos e implementação das classes Livro e CategoriaDTO

---
+++ biblioteca/src/main/java/com/sistema/biblioteca/dtos/LivroDTO.java
@@ -0,0 +1,75 @@
+package com.sistema.biblioteca.dtos;
+
+import com.sistema.biblioteca.enums.Tamanho;
+import com.sistema.biblioteca.models.Categoria;
+
+public class LivroDTO {
+
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private String texto;
+    private Categoria categoria;
+    private Tamanho tamanho;
+
+    public LivroDTO() {
+
+    }
+
+    public LivroDTO(Integer id, String titulo, String autor, String texto, Categoria categoria, Tamanho tamanho) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.texto = texto;
+        this.categoria = categoria;
+        this.tamanho = tamanho;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Tamanho getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Tamanho tamanho) {
+        this.tamanho = tamanho;
+    }
+}
